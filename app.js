3c37b1ff76aace67cb1c2f84e55f14dffcf7d041
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2022-06-24 16:55:41 -0500

    metodos express

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 /* Importar los modulos requeridos */
 
 const express = require('express');
+const bodyparser = require('body-parser');
 
 /* Configuracion Inicial */
 
 const app = express();
 const port = 3500;
-
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}))
 /* Iniciar las rutas */
 
 app.get("/prueba",function(req,res) {
@@ -16,6 +18,26 @@ app.get("/prueba",function(req,res) {
 
 app.get("/",function(req, res){
      res.send("hola")   
+});
+
+app.post("/usuarios",function (req, res) {
+    let cuerpo = req.body;
+    res.send(cuerpo.name);
+});
+
+app.get("/seleccionar", function (req,res) {
+    let query = req.query;
+    res.send(query);
+});
+
+app.put("/,actualizar/:nombre", function (req,res) {
+    let nombre = req.params.nombre;
+    let cuerpo = req.body;
+    res.send({
+        "nombre": nombre,
+        "cuerpo": cuerpo
+    })
+
 })
 
 /* Configurar el puerto  */
